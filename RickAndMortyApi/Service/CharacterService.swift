d9b1244c567f2e6cc3059aa599e403f607972738
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-18 12:04:40 +0200

    setting up the view model.

--- RickAndMortyApi/Service/CharacterService.swift
+++ RickAndMortyApi/Service/CharacterService.swift
@@ -7,29 +7,35 @@
 
 import Foundation
 
-enum CharacterServiceError: Error {
-    case failed
-    case failedToDecode
-    case invalidStatusCode
-}
-
-
+struct CharacterService{
     
-    func fetchCharacters() async throws -> [Character] {
+    enum CharacterServiceError: Error {
+        case failed
+        case failedToDecode
+        case invalidStatusCode
+    }
+
+
         
-        let url = URL(string: "https://rickandmortyapi.com/api/character")!
-        
-        //This await keyword ensures that we do not continue  until  we get that url. So it waits for that functionality.
-        //The try block allows to handle errors.
-        let(data, response) = try await URLSession.shared.data(from: url)
-       
-        guard let response = response as? HTTPURLResponse,
-              response.statusCode == 200 else {
-            throw CharacterServiceError.invalidStatusCode
+        func fetchCharacters() async throws -> [Character] {
+            
+            let url = URL(string: "https://rickandmortyapi.com/api/character")!
+            
+            //This await keyword ensures that we do not continue  until  we get that url. So it waits for that functionality.
+            //The try block allows to handle errors.
+            let(data, response) = try await URLSession.shared.data(from: url)
+           
+            guard let response = response as? HTTPURLResponse,
+                  response.statusCode == 200 else {
+                throw CharacterServiceError.invalidStatusCode
+            }
+            
+            let decodedData = try JSONDecoder().decode(CharacterServiceResult.self, from: data)
+            return decodedData.results
         }
         
-        let decodedData = try JSONDecoder().decode(CharacterServiceResult.self, from: data)
-        return decodedData.results
-    }
+
+
     
+}
 
---
+++ RickAndMortyApi/ViewModel/CharacterViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  CharacterViewModel.swift
+//  RickAndMortyApi
+//
+//  Created by Buhle Radzilani on 2024/06/18.
+//
+
+import Foundation
+
+class CharacterViewModel: ObservableObject {
+    
+    enum State {
+        case na
+        case loading
+        case success (data: [Character])
+        case failed(error: Error)
+    }
+    
+    @Published private(set) var state: State = .na
+    
+    private let service: CharacterService
+    
+    init(service: CharacterService) {
+        self.service = service
+    }
+    
+    
+    
+    func getCharacters() async{
+        
+        self.state = .loading
+        
+        do {
+            let characters = try await service.fetchCharacters()
+            self.state = .success(data: characters)
+        }catch{
+            self.state = .failed(error: error)
+        }
+        
+    }
+    
+}
