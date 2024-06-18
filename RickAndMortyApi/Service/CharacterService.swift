c9f30a036e49359627ff88a05b00c58835d4a032
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-18 11:51:03 +0200

    Finished setting up the service api.

---
+++ RickAndMortyApi/Service/CharacterService.swift
@@ -0,0 +1,35 @@
+//
+//  CharacterService.swift
+//  RickAndMortyApi
+//
+//  Created by Buhle Radzilani on 2024/06/18.
+//
+
+import Foundation
+
+enum CharacterServiceError: Error {
+    case failed
+    case failedToDecode
+    case invalidStatusCode
+}
+
+
+    
+    func fetchCharacters() async throws -> [Character] {
+        
+        let url = URL(string: "https://rickandmortyapi.com/api/character")!
+        
+        //This await keyword ensures that we do not continue  until  we get that url. So it waits for that functionality.
+        //The try block allows to handle errors.
+        let(data, response) = try await URLSession.shared.data(from: url)
+       
+        guard let response = response as? HTTPURLResponse,
+              response.statusCode == 200 else {
+            throw CharacterServiceError.invalidStatusCode
+        }
+        
+        let decodedData = try JSONDecoder().decode(CharacterServiceResult.self, from: data)
+        return decodedData.results
+    }
+    
+
