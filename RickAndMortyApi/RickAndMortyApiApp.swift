9c343a188b86569f9d2b4013b5c9c555520c52b9
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-18 11:01:09 +0200

    Initial Commit

---
+++ RickAndMortyApi/RickAndMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApiApp.swift
+//  RickAndMortyApi
+//
+//  Created by Buhle Radzilani on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
